c75d05897386e7930f53cf6de4bbf44d1a84d879
mitchellciupak <ciupak@purdue.edu>
2020-03-10 12:55:41 +0100

    Blockchain Mining

--- blockchain.py
+++ blockchain.py
@@ -8,34 +8,43 @@ Created on Mon Mar  9 21:11:15 2020
 # Flask v. 0.12.2
 # Postman v.7.20.0
 
-#Import
+
+# Module 1 - Create a Blockchain
+
+# To be installed:
+# Flask==0.12.2: pip install Flask==0.12.2
+# Postman HTTP Client: https://www.getpostman.com/
+
+# Importing the libraries
 import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
 
-#Init Blockchain
+# Part 1 - Building a Blockchain
+
 class Blockchain:
+
     def __init__(self):
         self.chain = []
-        self.createBlock(proof = 1, previous_hash = '0')
-        
+        self.create_block(proof = 1, previous_hash = '0')
+
     def create_block(self, proof, previous_hash):
-        block = {'index' : len(self.chain) + 1,
-                 'timestamp' : str(datetime.datetime.now()),
-                 'proof' : proof,
-                 'previous_hash' : previous_hash}
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
         self.chain.append(block)
         return block
-    
+
     def get_previous_block(self):
         return self.chain[-1]
-    
+
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            hash_operation = hashlib.sha256('new_proof**2 - previous_proof**2'.encode()).hexdigest()
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
@@ -43,26 +52,64 @@ class Blockchain:
         return new_proof
     
     def hash(self, block):
-        #Hash each block
-        #Makes block dictionary a string
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
-        
         while block_index < len(chain):
             block = chain[block_index]
             if block['previous_hash'] != self.hash(previous_block):
                 return False
-            
             previous_proof = previous_block['proof']
             proof = block['proof']
-            hash_operation = hashlib.sha256('proof**2 - previous_proof**2'.encode()).hexdigest()
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
         return True
-        
+
+# Part 2 - Mining our Blockchain
+
+#Create a Web App (https://flask.palletsprojects.com/en/1.1.x/quickstart/)
+app = Flask(__name__)
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block(): #http://127.0.0.1:5000/mine_block
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+@app.route('/get_chain', methods = ['GET']) #http://127.0.0.1:5000/get_chain
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET']) #http://127.0.0.1:5000/is_valid
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+    return jsonify(response), 200
+
+# Running the app
+app.run(host = '0.0.0.0', port = 5000)
